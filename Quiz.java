d3eefa8c0f7728bc653d2be055078822d49c809c
Jay <jfromentrudder@gmail.com>
2015-01-15 09:44:11 -0500

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,12 +1,17 @@
-public class Quiz {
   import java.util.*;
   import java.io.*;
   import javax.swing.*;
   import java.awt.*;
   import java.text.*;
+
+public class Quiz {
+
+	public static void main( String[] args ){
+
+	  JFrame frame = new JFrame();
+	  frame.setSize(800,800);
+	  frame.setVisible(true);
   
-  public Quiz() {
-  }
+	}	
   
-  }
 }
