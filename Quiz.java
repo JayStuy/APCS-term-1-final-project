739f06268c6a860821b2e332ecec38a29a79c7c1
Jay <jfromentrudder@gmail.com>
2015-01-22 01:29:44 -0500

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,17 +1,38 @@
-  import java.util.*;
-  import java.io.*;
-  import javax.swing.*;
-  import java.awt.*;
-  import java.text.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Quiz {
+public class Quiz extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+
+	public Quiz() {
+		setSize(600,600);//Size of JFrame
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);//Sets if its visible.
+
+        JButton startButton = new JButton("Start");
+        add(startButton);
+        startButton.addActionListener(this);
+		getContentPane().add(BorderLayout.SOUTH, startButton);
+		
+		JButton button2 = new JButton("Second");
+		add(button2);
+		button2.addActionListener(this);
+		getContentPane().add(BorderLayout.EAST, button2);
+	}
 
 	public static void main( String[] args ){
-
-	  JFrame frame = new JFrame();
-	  frame.setSize(800,800);
-	  frame.setVisible(true);
-  
+		new Quiz();
+		/*JFrame frame = new JFrame();
+		frame.setSize(800,800);
+		frame.setVisible(true);
+		*/
 	}	
+	
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Something");
+	}
   
 }
